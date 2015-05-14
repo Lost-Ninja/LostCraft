b5e553960283274e28a94b6d6a80b40e388de5c9
Lost-Ninja <legendary.lost.ninja@gmail.com>
2015-05-14 12:50:10 +0100

    Initial Offering
    Lost Craft Mod Pack (Private Pack)

---
+++ config/INpureProjects/custom_nei_filters/AgriCraft.js
@@ -0,0 +1,5 @@
+if (FML.isModLoaded("AgriCraft")) {
+    NEI.override("AgriCraft:waterChannel", [0]);
+    NEI.override("AgriCraft:waterChannelValve", [0]);
+    NEI.override("AgriCraft:waterTank", [0]);
+}
